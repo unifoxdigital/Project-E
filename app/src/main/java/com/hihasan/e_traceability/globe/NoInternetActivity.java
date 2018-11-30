6786ec56107ef1deffcdfa1c26750d8c407a42df
hihasan <hasan@hihasan.com>
2018-11-30 13:07:04 +0600

    java files

---
+++ app/src/main/java/com/hihasan/e_traceability/globe/NoInternetActivity.java
@@ -0,0 +1,13 @@
+package com.hihasan.e_traceability.globe;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class NoInternetActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/hihasan/e_traceability/globe/StartupActivity.java
@@ -0,0 +1,14 @@
+package com.hihasan.e_traceability.globe;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartupActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
