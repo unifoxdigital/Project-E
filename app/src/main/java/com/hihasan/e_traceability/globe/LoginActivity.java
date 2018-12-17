3bcbede2abc39fccae774032e7a6a8d212244a6c
hihasan <hasan@hihasan.com>
2018-12-17 19:46:44 +0600

    layer

--- app/src/main/java/com/hihasan/e_traceability/globe/LoginActivity.java
+++ app/src/main/java/com/hihasan/e_traceability/globe/LoginActivity.java
@@ -4,12 +4,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class StartupActivity extends AppCompatActivity
+public class LoginActivity extends AppCompatActivity
 {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_startup);
-        
+        setContentView(R.layout.activity_login);
     }
 }
